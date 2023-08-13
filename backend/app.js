6695a976249bcccdbb4530bdde1d4485ffd71441
Anatoly SIdash <sidash85@yandex.ru>
2023-08-13 19:56:24 +0300

    fix: fix CORS mistakes

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,9 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 const app = express();
 
 app.use(cors({
-  origin: 'https://mesto.sidash.nomoreparties.co',
+  origin: ['http://localhost:3000', 'https://mesto.sidash.nomoreparties.co'],
   credentials: true,
+  maxAge: 30,
 }));
 
 app.use(helmet());
